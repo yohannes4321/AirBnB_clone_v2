acfa0b0afc52b457582e8162ace9b0bed308b107
yohannes4321 <alemuyohannes960@gmail.com>
2024-06-30 00:23:23 +0300

    mysql

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,7 +1,7 @@
---prepares a MYSQL SERVER
+-- prepares a MySQL server for the project
 
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
-CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED by "hbnb_test_pwd";
-GRANT ALL PRIVILEGS ON hbnb_test_db.* To 'hbnb_test'@'localhost';
-GRANT SELECT ON performance_schema.* To 'hbnb_test'@'localhost';
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+GRANT ALL PRIVILEGES ON `hbnb_test_db`.* TO 'hbnb_test'@'localhost';
+GRANT SELECT ON `performance_schema`.* TO 'hbnb_test'@'localhost';
 FLUSH PRIVILEGES;
