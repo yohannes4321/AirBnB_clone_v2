efc65166584c9198bf1c1c3f10f6c4832d31c811
yohannes4321 <alemuyohannes960@gmail.com>
2024-07-29 00:07:39 +0300

    flask m

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-
+"""
+starts a Flask web application
+"""
 from flask import Flask
 
 app = Flask(__name__)
@@ -11,4 +13,4 @@ def index():
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host='0.0.0.0', port='5000')
