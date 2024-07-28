2f5baecf9a8cf0617920c04a5d03ae270094c9eb
yohannes4321 <alemuyohannes960@gmail.com>
2024-07-29 02:37:56 +0300

    flusk

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -38,5 +38,10 @@ def imanumber(n):
     return "{:d} is a number".format(n)
 
 
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def tem(n):
+    return render_template('5-number.html', n=n)
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
