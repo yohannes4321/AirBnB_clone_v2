f15f0853572de129c1b9977e2eb9cd6401609da0
yohannes4321 <alemuyohannes960@gmail.com>
2024-07-29 03:17:44 +0300

    flusk

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,20 +1,22 @@
 #!/usr/bin/python3
-"""Flask app to generate html list of all states from storage"""
+"""Flask app to generate HTML list of all states from storage"""
 from flask import Flask, render_template
 from models import storage
+from models.state import State
+
 app = Flask('web_flask')
 app.url_map.strict_slashes = False
 
 
 @app.route('/states_list')
 def list_of_states():
-    """Render html with unordered list of states from `storage`"""
-    states = sorted(storage.all('State').values(), key=lambda s: s.name)
+    """Render HTML with unordered list of states from `storage`"""
+    states = sorted(storage.all(State).values(), key=lambda s: s.name)
     return render_template('7-states_list.html', states=states)
 
 
 @app.teardown_appcontext
-def teardown_db(*args, **kwargs):
+def teardown_db(exception):
     """Close database or file storage"""
     storage.close()
 
